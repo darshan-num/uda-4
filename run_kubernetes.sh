e08ea62214457d4304063af9fac72b6d938b091e
Darshan Sheth <darshan.sheth@numerator.com>
2021-02-09 15:20:37 +0300

    updated readme & added kubernetes logs

--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,17 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
-
+dockerpath=dashnum/dash-uda-4:commit1
+podname=flaskdemo1
 # Step 2
 # Run the Docker Hub container with kubernetes
+kubectl run $podname --image=$dockerpath --port=80 --labels app=$podname
 
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward $podname 8000:80
